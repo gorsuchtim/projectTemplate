540a2b37e2f31171329b3316e614f81358fda4cf
Tim <tgorsuch@redventures.com>
2020-08-18 15:26:06 -0400

    updates componentTemplate structure

--- src/js/Components/ComponentIndex.js
+++ src/js/Components/ComponentIndex.js
@@ -2,13 +2,16 @@
  * @desc This function runs on page load and iterates through the key:value pairs in asyncModules
  * It looks for an element on the page with an attribute that matches the key and loads & runs the JS file associated to that element
  * @param // const asyncModules accepts the key:value pair as str.  Ex: ".myClassName": "myFileToRun"
- * The associated file must be in /src/js/Components and have a folder named the same as the file.  Ex: /Components/MyTestComponent/MyTestComponent.js
+ * The associated file must be in /src/js/Components
+ * and must have a folder named the same as the file.  Ex: /Components/ComponentTemplate/ComponentTemplate.js
  *
  */
 const ComponentIndex = () => {
   // Key: value pair of element to target on the page: js component to run when element is on the page
-  // Enter as strings ".myClass": "myjavascriptfile"
-  const asyncModules = {};
+  // Enter as strings ".component-template": "ComponentTemplate"
+  const asyncModules = {
+    ".component-template": "ComponentTemplate"
+  };
 
   const loadModules = async () => {
     const imports = Object.entries(asyncModules);
