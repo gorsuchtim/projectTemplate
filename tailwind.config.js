048e6b61eef3cb3d0bc033efddd6caff0ccb6804
Tim <tgorsuch@redventures.com>
2020-09-16 11:55:53 -0400

    adds gradients to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     screens: {
       sm: "460px",
       md: "768px",
-      lg: "1280px"
+      lg: "1280px",
     },
     colors: {
       transparent: "transparent",
@@ -37,7 +37,7 @@ module.exports = {
       "red-error": "#fdd",
       "red-vivid": "#f00",
       "yellow-soft": "#feea6e",
-      "yellow-warning": "#fff8c9"
+      "yellow-warning": "#fff8c9",
     },
     spacing: {
       full: "100%",
@@ -68,11 +68,11 @@ module.exports = {
       104: "104px",
       120: "120px",
       144: "144px",
-      476: "476px"
+      476: "476px",
     },
-    borderColor: theme => ({
+    borderColor: (theme) => ({
       ...theme("colors"),
-      default: theme("colors['gray-heather']", "currentColor")
+      default: theme("colors['gray-heather']", "currentColor"),
     }),
 
     borderRadius: {
@@ -82,23 +82,23 @@ module.exports = {
       4: "4px",
       6: "6px",
       8: "8px",
-      16: "16px"
+      16: "16px",
     },
 
     borderWidth: {
       default: "1px",
-      "0": "0",
-      "2": "2px",
-      "3": "3px",
-      "4": "4px",
-      "8": "8px"
+      0: "0",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      8: "8px",
     },
 
     boxShadow: {
       none: "none",
       hover:
         "0 2px 4px -1px rgba(50, 80, 112, 0.2), 0 8px 10px 1px rgba(50, 80, 112, 0.14), 0 3px 14px 2px rgba(50, 80, 112, 0.12)",
-      1: "0 2px 4px -1px rgba(50, 80, 112, 0.2), 0 4px 6px 0 rgba(50, 80, 112, 0.14), 0 1px 10px 0 rgba(50, 80, 112, 0.12)"
+      1: "0 2px 4px -1px rgba(50, 80, 112, 0.2), 0 4px 6px 0 rgba(50, 80, 112, 0.14), 0 1px 10px 0 rgba(50, 80, 112, 0.12)",
     },
 
     fontSize: {
@@ -121,7 +121,7 @@ module.exports = {
       40: "40px",
       48: "48px",
       64: "64px",
-      92: "92px"
+      92: "92px",
     },
 
     fontWeight: {
@@ -133,46 +133,50 @@ module.exports = {
       600: "600",
       700: "700",
       800: "800",
-      900: "900"
+      900: "900",
     },
 
-    height: theme => ({
+    gradientColorStops: (theme) => ({
+      ...theme("colors"),
+    }),
+
+    height: (theme) => ({
       auto: "auto",
       ...theme("spacing"),
       full: "100%",
-      screen: "100vh"
+      screen: "100vh",
     }),
 
     margin: (theme, { negative }) => ({
       auto: "auto",
       ...theme("spacing"),
-      ...negative(theme("spacing"))
+      ...negative(theme("spacing")),
     }),
 
-    maxHeight: theme => ({
+    maxHeight: (theme) => ({
       none: "none",
       full: "100%",
       screen: "100vh",
-      ...theme("spacing")
+      ...theme("spacing"),
     }),
 
     maxWidth: (theme, { breakpoints }) => ({
       none: "none",
       full: "100%",
       ...theme("spacing"),
-      ...breakpoints(theme("screens"))
+      ...breakpoints(theme("screens")),
     }),
 
-    minHeight: theme => ({
-      "0": "0",
+    minHeight: (theme) => ({
+      0: "0",
       full: "100%",
-      ...theme("spacing")
+      ...theme("spacing"),
     }),
 
-    minWidth: theme => ({
-      "0": "0",
+    minWidth: (theme) => ({
+      0: "0",
       full: "100%",
-      ...theme("spacing")
+      ...theme("spacing"),
     }),
 
     opacity: {
@@ -189,16 +193,16 @@ module.exports = {
       65: "0.65",
       70: "0.70",
       75: "0.75",
-      100: "1"
+      100: "1",
     },
 
-    padding: theme => theme("spacing"),
+    padding: (theme) => theme("spacing"),
     space: (theme, { negative }) => ({
       ...theme("spacing"),
-      ...negative(theme("spacing"))
+      ...negative(theme("spacing")),
     }),
 
-    width: theme => ({
+    width: (theme) => ({
       auto: "auto",
       ...theme("spacing"),
       "1/2": "50%",
@@ -228,23 +232,23 @@ module.exports = {
       "10/12": "83.333333%",
       "11/12": "91.666667%",
       full: "100%",
-      screen: "100vw"
+      screen: "100vw",
     }),
 
     scale: {
-      "0": "0",
-      "50": ".5",
-      "75": ".75",
-      "80": ".8",
-      "90": ".9",
-      "95": ".95",
-      "100": "1",
-      "105": "1.05",
-      "110": "1.1",
-      "125": "1.25",
-      "130": "1.3",
-      "150": "1.5"
-    }
+      0: "0",
+      50: ".5",
+      75: ".75",
+      80: ".8",
+      90: ".9",
+      95: ".95",
+      100: "1",
+      105: "1.05",
+      110: "1.1",
+      125: "1.25",
+      130: "1.3",
+      150: "1.5",
+    },
   },
-  variants: {}
+  variants: {},
 };
