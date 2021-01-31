bb8b08562adae0e09e47d5185eca51e5ca51343a
Tim <tgorsuch@redventures.com>
2021-01-31 16:34:59 -0500

    adds resolve/alias to module exports

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,40 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const path = require("path");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "App.dev.js",
+    filename: "App.dev.js"
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/html/index.html",
-    }),
+      template: "./src/html/index.html"
+    })
   ],
   module: {
     rules: [
       {
         test: /\.(css|scss)$/,
         use: [
-            MiniCssExtractPlugin.loader,
-            "css-loader?-url",
-            {
-                loader: "postcss-loader",
-            },
-            {
-                loader: "sass-loader",
-            },
-        ],
-      },
-    ],
+          MiniCssExtractPlugin.loader,
+          "css-loader?-url",
+          {
+            loader: "postcss-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
   },
+  resolve: {
+    alias: {
+      Utilities: path.resolve(__dirname, "src/js/Utilities/"),
+      Components: path.resolve(__dirname, "src/js/Components/")
+    },
+    extensions: [".js", ".jsx", ".json"]
+  }
 };
